22d816e88b55b18fa34b285dd272cfcd58b15660
Richard Haines <richard.haines@88.131.110>
2019-10-22 17:04:33 +0200

    test fix

--- web/.cache/api-runner-browser-plugins.js
+++
@@ -1,7 +0,0 @@
-module.exports = [{
-      plugin: require('D:/HUNGRY BEAR STUDIO/hungry-bear-studio/web/node_modules/gatsby-plugin-theme-ui/gatsby-browser.js'),
-      options: {"plugins":[]},
-    },{
-      plugin: require('D:/HUNGRY BEAR STUDIO/hungry-bear-studio/web/node_modules/gatsby-plugin-offline/gatsby-browser.js'),
-      options: {"plugins":[]},
-    }]
